368dc68ee431a46ba09657c4c571357594cba119
Michael Wright <mkwright@gmail.com>
2017-03-03 09:40:37 -0700

    Added completed simple project for inc/dec number

--- project/src/Main.elm
+++ project/src/Main.elm
@@ -1,8 +1,27 @@
 import Html exposing (..)
+import Html.Events exposing (onClick) 
 
-main = 
+type Message = Increment | Decrement 
+
+model = 0
+
+view model = 
     div []
-        [ button [] [ text "-" ]
-        , text "Hello World"
-        , button [] [ text "+" ]
+        [ button [ onClick Decrement ] [ text "-" ]
+        , model |> toString |> text 
+        , button [ onClick Increment ] [ text "+" ]
         ]
+
+update msg model =
+    case msg of 
+        Increment -> 
+            model + 1
+        Decrement ->
+            model - 1
+
+main =
+    beginnerProgram
+     { model = model
+     , view = view 
+     , update = update 
+     }
